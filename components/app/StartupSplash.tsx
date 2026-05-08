530d25f317cac4d95e5ecce3027bf6606ada6490
mmiskatul <masabimiskat@gmail.com>
2026-05-08 06:26:51 +0600

    update the whole system

--- components/app/StartupSplash.tsx
+++ components/app/StartupSplash.tsx
@@ -1,11 +1,44 @@
-import { Image, StyleSheet, View } from "react-native";
+import { StyleSheet, View, Animated } from "react-native";
+import { useEffect, useRef } from "react";
 
 const splashImage = require("../../assets/images/image.png");
 
 export default function StartupSplash() {
+  const scaleAnim = useRef(new Animated.Value(0.3)).current;
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        tension: 20,
+        friction: 6,
+        useNativeDriver: true,
+      }),
+      Animated.spring(rotateAnim, {
+        toValue: 1,
+        tension: 20,
+        friction: 6,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  const spin = rotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["-45deg", "0deg"],
+  });
+
   return (
     <View style={styles.container}>
-      <Image source={splashImage} style={styles.logo} resizeMode="contain" />
+      <Animated.Image 
+        source={splashImage} 
+        style={[
+          styles.logo,
+          { transform: [{ scale: scaleAnim }, { rotate: spin }] }
+        ]} 
+        resizeMode="contain" 
+      />
     </View>
   );
 }
